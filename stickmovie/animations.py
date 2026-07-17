177f7c10979eb00181933f6492c69bac1339ee35
Rushaun6 <166856723+Rushaun6@users.noreply.github.com>
2026-07-17 14:02:11 -0500

    Add Animation class for character animations

--- stickmovie/animations.py
+++ stickmovie/animations.py
@@ -1,1 +1,15 @@
-# Animation sequences and keyframes
+class Animation:
+    def __init__(self, character, speed=6):
+        self.character = character
+        self.speed = speed
+        self.frame = 0
+
+    def update(self):
+        self.frame += 1
+
+        if self.frame >= self.speed:
+            self.frame = 0
+            self.character.next_pose()
+
+    def sprite(self):
+        return self.character.pose()
