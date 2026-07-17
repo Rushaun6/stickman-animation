72114f668cc1f00eb51f5f0515ac1de7560bb99d
Rushaun6 <166856723+Rushaun6@users.noreply.github.com>
2026-07-17 14:01:12 -0500

    Add Character class with pose management

--- stickmovie/characters.py
+++ stickmovie/characters.py
@@ -1,1 +1,41 @@
-# Character definitions and management
+class Character:
+    def __init__(self, name, poses):
+        self.name = name
+        self.poses = poses
+        self.current_pose = 0
+
+    def pose(self):
+        return self.poses[self.current_pose]
+
+    def next_pose(self):
+        self.current_pose += 1
+
+        if self.current_pose >= len(self.poses):
+            self.current_pose = 0
+
+
+HERO = Character(
+    "hero",
+    [
+        r"""
+  o
+ /|\
+ / \
+""",
+        r"""
+ \o
+  |\
+ / \
+""",
+        r"""
+  o/
+ /|
+ / \
+""",
+        r"""
+ \o/
+  |
+ / \
+"""
+    ]
+)
