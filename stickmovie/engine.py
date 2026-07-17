58a977c45763f6c4d9145bc30deb8c144b53cf5b
Rushaun6 <166856723+Rushaun6@users.noreply.github.com>
2026-07-17 13:58:50 -0500

    Implement core animation engine with FPS control

--- stickmovie/engine.py
+++ stickmovie/engine.py
@@ -1,1 +1,18 @@
-# Core animation engine
+import time
+
+from renderer import clear_screen
+from scenes import intro_scene
+
+
+FPS = 12
+
+
+def start():
+    while True:
+        clear_screen()
+
+        frame = intro_scene()
+
+        print(frame)
+
+        time.sleep(1 / FPS)
